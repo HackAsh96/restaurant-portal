61c59ee221b28fe41b7ed8b7eaf34d7676d79f71
Abed Sujan <abedsujan@gmail.com>
2018-10-07 16:01:10 +0200

    show openning hour for restaurants

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let restaurants = [{
         menu: 'Indian Spicy Indian Spicy Indian SpicyIndian Spicy',
         open: 17,
         close: 22,
-        isOpen: false
+        isOpen: true
     },
     {
         restaurantId: 3,
@@ -44,7 +44,7 @@ let restaurants = [{
         restaurantId: 6,
         name: 'Hot spicy',
         menu: 'Hot spicy Hot spicy Hot spicy Hot spicy Hot spicy',
-        open: 18,
+        open: 10,
         close: 22,
         isOpen: false
     }
@@ -56,18 +56,45 @@ console.log(restaurants);
 
 // Implement with fuction 
 function showStatus(restaurant) {
-console.log('restaurant', restaurant);
+    
     if (restaurant.isOpen) {
+         // When restaurant is open
+
         document.getElementById('status-' + restaurant.restaurantId).innerHTML = `
         <span class="status-open"></span> Open
         `;
+
+        document.getElementById('button-' + restaurant.restaurantId).innerHTML = 
+        `
+        <button type="button" class="btn btn-sm btn-outline-secondary">Menu</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary">Order</button>
+
+            `;
+
     } else {
+
+        // When restaurant is closed
+
         document.getElementById('status-' + restaurant.restaurantId).innerHTML = `
     <span class="status-closed"></span> Closed
         `;
+
+        document.getElementById('button-' + restaurant.restaurantId).innerHTML = 
+        `
+        <button type="button" 
+            class="btn btn-sm btn-outline-secondary" 
+            onclick="showOpeningHour(${restaurant.restaurantId})">
+            Opening Hour
+        </button>
+
+            `;
     }
 }
 
 for(let i=0; i<restaurants.length; i++){
     showStatus(restaurants[i]);
+}
+
+function showOpeningHour(restaurantId){
+    alert('Open: ' + restaurants[restaurantId-1].open + ' close: '+restaurants[restaurantId-1].close );
 }
